648602074d3b2a6a0e650573db27a32dc8f825b5
VictorPinargote <joelpinargote7@gmail.com>
2026-01-11 23:54:03 -0500

    commit creacion de modelos

--- Tienda_Bitforge/gestion/models.py
+++ Tienda_Bitforge/gestion/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+#Modelo de Categoria
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True, null=True) #blank=True y null=True permiten que el campo sea opcional y nulo
+    icono = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Categorias" #Nombre en singular en la admin
+
+#Modelo de Proveedor
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    direccion = models.CharField(max_length=100)
+    activo = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "Proveedores"
